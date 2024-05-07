9576f136edf6474c86f3f7091e0458b29b4365b9
Vigneshwar Rajesh <vignesh.rajesh@mail.utoronto.ca>
2024-05-06 21:13:00 -0400

    create main method

--- pika_test/test_rabbit_mq_recieve.py
+++ pika_test/test_rabbit_mq_recieve.py
@@ -1,18 +1,25 @@
-import pika
+import pika, sys, os
 
-def callback(ch, method, properties, body):
-    print(f" [x] Received {body}")
+def main():
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
+    channel.queue_declare(queue='hello')
 
-# Declare the same queue from where to consume messages
-channel.queue_declare(queue='hello')
+    def callback(ch, method, properties, body):
+        print(f" [x] Received {body}")
 
-# Set up subscription on the queue
-channel.basic_consume(queue='hello',
-                      on_message_callback=callback,
-                      auto_ack=True)
+    channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 
-print(' [*] Waiting for messages. To exit press CTRL+C')
-channel.start_consuming()
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
