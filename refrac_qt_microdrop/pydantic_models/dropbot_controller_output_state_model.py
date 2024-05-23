7839a67d5af82a26f4683372e7766ee453a34e1c
Miguel <Miguel.Weerasinghe@mail.utoronto.ca>
2024-05-22 23:59:36 -0400

    Payload models for each of the logic files to actually have messages that are validated and can craft and send validated messages very simply.

---
+++ refrac_qt_microdrop/pydantic_models/dropbot_controller_output_state_model.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+class DBOutputStateModel(BaseModel):
+    Signal: str
+    OutputState: bool
+
+class DBChannelsChangedModel(BaseModel):
+    Signal: str
+    Channels: str
+
+class DBVoltageChangedModel(BaseModel):
+    Signal: str
+    Voltage: str
+
+class DBChannelsMetastateChanged(BaseModel):
+    Signal: str
+    Drops: str
