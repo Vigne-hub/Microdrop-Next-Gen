2295ccb4457f77383fe951e674808867e16f3442
Miguel <Miguel.Weerasinghe@mail.utoronto.ca>
2024-05-24 05:28:58 -0400

    just deleting old directory not in use

--- mig_testing_envisage/backends/broker.py
+++
@@ -1,6 +0,0 @@
-# broker.py
-import dramatiq
-from dramatiq.brokers.rabbitmq import RabbitmqBroker
-
-rabbitmq_broker = RabbitmqBroker(url="amqp://guest:guest@localhost:5672/")
-dramatiq.set_broker(rabbitmq_broker)
