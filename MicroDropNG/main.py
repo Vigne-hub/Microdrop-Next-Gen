f586d1bd6561000229cfa2ae30a464ec2cc02bb5
Miguel <Miguel.Weerasinghe@mail.utoronto.ca>
2024-06-03 16:12:20 -0400

    Refractored to only have added dropbot controller logic related code for PR

--- MicroDropNG/main.py
+++ MicroDropNG/main.py
@@ -2,15 +2,13 @@
 import sys
 from PySide6.QtWidgets import QApplication
 from envisage.api import Application
-from MicroDropNG.plugins.backend_plugins.dropbot_controller import DropbotControllerPlugin
-from MicroDropNG.plugins.frontend_plugins.dropbot_test_GUI import DropbotGUIPlugin
-from MicroDropNG.plugins.control_plugins.event_hub import EventHubPlugin
+
 
 
 def main():
     app = QApplication(sys.argv)
 
-    plugins = [DropbotControllerPlugin(), EventHubPlugin(), DropbotGUIPlugin()]
+    plugins = []
     envisage_app = Application(plugins=plugins)
     envisage_app.start()
 
