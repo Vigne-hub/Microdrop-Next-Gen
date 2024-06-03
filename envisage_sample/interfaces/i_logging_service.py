65789acff10da7a60c76e60cb2b9a51542dcdce5
Miguel <Miguel.Weerasinghe@mail.utoronto.ca>
2024-06-03 05:46:42 -0400

    Committing to make PR based on the specific isolated components. Assuming pub/sub passes, this is pub sub + dropbot code so after dropbot logic has been added and tested.
    In next branch I will add plugin structure and event hub and base front end ui for ISSUE #29

--- envisage_sample/interfaces/i_logging_service.py
+++
@@ -1,6 +0,0 @@
-from traits.has_traits import Interface
-
-
-class ILoggingService(Interface):
-    def log(self, message):
-        """Log the given message."""
--- envisage_sample/services/__init__.py
+++
@@ -1,3 +0,0 @@
-##### Each of these services would need to have a model to define their payload which can be imported to use this service ####
-
-
