c89d09c25cbcf56761202398d8a7211906a44abb
Miguel <Miguel.Weerasinghe@mail.utoronto.ca>
2024-06-04 08:33:12 -0400

    Update main to now add each of the 4 plugins (dropbot controller, pubsubmanager, event_hub, and dropbot test gui)

--- MicroDropNG/main.py
+++ MicroDropNG/main.py
@@ -3,11 +3,16 @@ import sys
 from PySide6.QtWidgets import QApplication
 from envisage.api import Application
 
+from plugins.control_plugins.event_hub import EventHubPlugin
+from plugins.backend_plugins.dropbot_controller import DropbotControllerPlugin
+from plugins.frontend_plugins.dropbot_test_GUI import DropbotGUIPlugin
+from plugins.utility_plugins.pub_sub_manager_plugin import PubSubManagerPlugin
+
 
 def main():
     app = QApplication(sys.argv)
 
-    plugins = []
+    plugins = [PubSubManagerPlugin(), DropbotControllerPlugin(), EventHubPlugin(), DropbotGUIPlugin()]
     envisage_app = Application(plugins=plugins)
     envisage_app.start()
 
