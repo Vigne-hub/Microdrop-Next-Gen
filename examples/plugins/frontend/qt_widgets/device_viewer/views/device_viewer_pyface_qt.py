7634cc4eaa1dfe7f19197bbbdca636971133fc01
Vigneshwar Rajesh <vignesh.rajesh@mail.utoronto.ca>
2024-06-07 15:27:35 -0400

    changed absolute to relative import

--- examples/plugins/frontend/qt_widgets/device_viewer/views/device_viewer_pyface_qt.py
+++ examples/plugins/frontend/qt_widgets/device_viewer/views/device_viewer_pyface_qt.py
@@ -9,8 +9,8 @@ import os
 import logging
 
 # local imports
-from examples.plugins.frontend.qt_widgets.device_viewer.views.electrodes_view import ElectrodeLayer
-from examples.plugins.frontend.qt_widgets.device_viewer.utils.auto_fit_graphics_view import AutoFitGraphicsView
+from .electrodes_view import ElectrodeLayer
+from ..utils.auto_fit_graphics_view import AutoFitGraphicsView
 
 logger = logging.getLogger(__name__)
 
