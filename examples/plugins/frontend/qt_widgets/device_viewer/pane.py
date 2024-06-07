9e69d0be643b8e7b478b3d4a7a2b8ed443b7f8ef
Vigneshwar Rajesh <vignesh.rajesh@mail.utoronto.ca>
2024-06-07 16:51:23 -0400

    simple pane to view device

--- examples/plugins/frontend/qt_widgets/device_viewer/pane.py
+++ examples/plugins/frontend/qt_widgets/device_viewer/pane.py
@@ -1,25 +1,69 @@
 # enthought imports
 from traits.api import Instance
 from pyface.tasks.api import TaskPane
-from pyface.qt.QtWidgets import QWidget, QVBoxLayout
+from pyface.qt.QtGui import QWidget, QVBoxLayout, QGraphicsScene
+from pyface.qt.QtOpenGLWidgets import QOpenGLWidget
+from pyface.qt.QtCore import Qt
 
 # local imports
-from .views.device_viewer_pyface_qt import DeviceViewerWidget
+from .views.electrodes_view import ElectrodeLayer
+from .utils.auto_fit_graphics_view import AutoFitGraphicsView
+from .. import initialize_logger
+
+logger = initialize_logger(__name__)
+
+# system imports
+import os
 
 
 class DeviceViewerPane(TaskPane):
+    """
+    A widget for viewing the device. This puts the electrode layer into a graphics view.
+    """
 
-    # traits
-    viewer = Instance(DeviceViewerWidget)
+    # ----------- traits ---------------------
 
-    # traits interface
-    def _viewer_default(self):
-        return DeviceViewerWidget()
+    scene = Instance(QGraphicsScene)
+    view = Instance(AutoFitGraphicsView)
+    current_electrode_layer = Instance(ElectrodeLayer, allow_none=True)
+
+    # --------- trait initializers -------------
+    def _scene_default(self):
+        return QGraphicsScene()
+
+    def _view_default(self):
+        view = AutoFitGraphicsView(self.scene)
+        view.setObjectName('device_view')
+        view.setViewport(QOpenGLWidget())
+
+        return view
+
+    # ------- trait change handlers -----------------
+    def _svg_path_changed(self, new_path):
+        """
+        Trigger an update to redraw and re-initialize the svg widget once a new svg file is selected.
+        """
+        self.current_electrode_layer = ElectrodeLayer("layer1", new_path)
+        self.remove_current_layer()
+        self.scene.addItem(self.current_electrode_layer)
+        logger.debug(f"Layer {self.current_electrode_layer.id} added -> {new_path}")
+        self.scene.setSceneRect(self.scene.itemsBoundingRect())
+        self.view.fitInView(self.scene.sceneRect(), Qt.AspectRatioMode.KeepAspectRatio)
+        self.svg_path_button = os.path.basename(new_path)
+
+    # ------- class methods -------------------------
+    def remove_current_layer(self):
+        """
+        Utility methods to remove current scene's electrode layer.
+        """
+        if self.current_electrode_layer:
+            self.scene.removeItem(self.current_electrode_layer)
+            self.scene.clear()
+            self.scene.update()
 
     # ITaskPane interface
     def create(self, parent):
         self.control = QWidget(parent)
         layout = QVBoxLayout()
-        layout.addWidget(self.viewer.view)
-        layout.addWidget(self.viewer.svg_path_button)
+        layout.addWidget(self.view)
         self.control.setLayout(layout)
--- examples/plugins/frontend/qt_widgets/device_viewer/utils/auto_fit_graphics_view.py
+++ examples/plugins/frontend/qt_widgets/device_viewer/utils/auto_fit_graphics_view.py
@@ -1,7 +1,5 @@
 from pyface.qt.QtWidgets import QGraphicsView
 from pyface.qt.QtCore import Qt
-from traitsui.basic_editor_factory import BasicEditorFactory
-from traitsui.qt.editor import Editor
 
 from ... import initialize_logger
 
@@ -12,6 +10,7 @@ class AutoFitGraphicsView(QGraphicsView):
     """
     A QGraphicsView that automatically fits the scene rect when the view is resized
     """
+
     def resizeEvent(self, event):
         logger.debug(f"Resizing view size: {self.scene().sceneRect()}")
         self.fitInView(self.scene().sceneRect().adjusted(20, 20, 20, 20), Qt.AspectRatioMode.KeepAspectRatio)
