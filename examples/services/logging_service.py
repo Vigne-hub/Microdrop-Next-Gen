58ebf4294a3cfbc981064ad90fe6742313134967
Vigneshwar Rajesh <vignesh.rajesh@mail.utoronto.ca>
2024-06-09 20:11:42 -0400

    Cleanup: More extra directories removed

--- examples/services/logging_service.py
+++
@@ -1,9 +0,0 @@
-from traits.has_traits import provides
-
-from ..interfaces.i_logging_service import ILoggingService
-
-
-@provides(ILoggingService)
-class LoggingService:
-    def log(self, message):
-        print(f"Log: {message}")
