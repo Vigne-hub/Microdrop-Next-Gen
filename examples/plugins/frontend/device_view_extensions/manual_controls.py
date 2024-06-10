a0fa7586be7294c3858af0b98757551b200b6ba3
Vigneshwar Rajesh <vignesh.rajesh@mail.utoronto.ca>
2024-06-10 01:34:10 -0400

    Manual control MVC.

---
+++ examples/plugins/frontend/device_view_extensions/manual_controls.py
@@ -0,0 +1,43 @@
+from traits.api import HasTraits, Range
+from traitsui.api import View, Group, Item, Controller
+
+
+class ManualControlModel(HasTraits):
+    voltage = Range(0, 1000, desc="the voltage to set on the dropbot device")
+    frequency = Range(0, 100000, desc="the frequency to set on the dropbot device")
+
+
+ManualControlView = View(
+    Group(
+        Item(
+            name='voltage',
+            label='Voltage (V)',
+        ),
+
+        Item(
+            name='frequency',
+            label='Frequency (Hz)',
+        ),
+    ),
+    title='Manual Controls',
+    resizable=True,
+)
+
+
+class ManualControlControl(Controller):
+
+    def voltage_setattr(self, info, object, traitname, value):
+        print(f"Voltage changed to {value}")
+        return super().setattr(info, object, traitname, value)
+
+    def frequency_setattr(self, info, object, traitname, value):
+        print(f"Frequency changed to {value}")
+        return super().setattr(info, object, traitname, value)
+
+
+if __name__ == "__main__":
+    model = ManualControlModel()
+    view = ManualControlView
+    controller = ManualControlControl(model=model)
+
+    model.configure_traits(view=view, handler=controller)
