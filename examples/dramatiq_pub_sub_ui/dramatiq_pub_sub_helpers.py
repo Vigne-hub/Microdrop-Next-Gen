8fccf41b1e9037b909f47f339ce3d26be380ae32
Vigneshwar Rajesh <vignesh.rajesh@mail.utoronto.ca>
2024-06-13 01:38:40 -0400

    add pytests to the message routing system.

--- examples/dramatiq_pub_sub_ui/dramatiq_pub_sub_helpers.py
+++ examples/dramatiq_pub_sub_ui/dramatiq_pub_sub_helpers.py
@@ -1,4 +1,4 @@
-from traits.api import HasTraits, Dict, Str, List
+from traits.api import HasTraits, Dict, Str, List, Callable
 import dramatiq
 from _logger import get_logger
 
@@ -26,35 +26,65 @@ def publish_message(message, topic, actor_to_send="message_router_actor"):
 
 class MessageRouterData(HasTraits):
     """
-    A class that stores topics and their subscribers
+    A class that stores topics and their subscribers.
+
+    Attributes:
+        topic_subscriber_map (Dict): A dictionary mapping topics to a list of their subscribing actor names.
     """
     topic_subscriber_map = Dict(Str, List(Str),
-                                desc=
-                                "A dictionary of topics and a list of their subscribing actor names"
-                                )
+                                desc="A dictionary of topics and a list of their subscribing actor names")
 
-    def add_subscriber_to_topic(self, topic, subscribing_actor_name):
+    def add_subscriber_to_topic(self, topic: Str, subscribing_actor_name: Str):
+        """
+        Adds a subscriber to a specific topic.
+
+        Args:
+            topic (str): The topic to subscribe to.
+            subscribing_actor_name (str): The name of the subscribing actor.
+        """
         if topic not in self.topic_subscriber_map:
             self.topic_subscriber_map[topic] = [subscribing_actor_name]
         else:
-            self.topic_subscriber_map[topic].append(subscribing_actor_name)
+            if subscribing_actor_name not in self.topic_subscriber_map[topic]:
+                self.topic_subscriber_map[topic].append(subscribing_actor_name)
 
-    def remove_subscriber_from_topic(self, topic, subscribing_actor_name):
+    def remove_subscriber_from_topic(self, topic: Str, subscribing_actor_name: Str):
+        """
+        Removes a subscriber from a specific topic. If the topic has no more subscribers, it is removed from the map.
+
+        Args:
+            topic (str): The topic to unsubscribe from.
+            subscribing_actor_name (str): The name of the subscribing actor.
+        """
         if topic in self.topic_subscriber_map:
             self.topic_subscriber_map[topic].remove(subscribing_actor_name)
             if not self.topic_subscriber_map[topic]:
                 del self.topic_subscriber_map[topic]
 
     def get_subscribers_for_topic(self, topic):
+        """
+        Gets the list of subscribers for a specific topic. A subscriber to a topic 'x' is also a subscriber to 'x.y.z',
+        but a subscriber to 'x.y.z' is not a subscriber to 'x'.
+
+        Args:
+            topic (str): The topic to get subscribers for.
+
+        Returns:
+            list: A list of subscribers for the topic.
+        """
+        subscribers = set()
+        topic_parts = topic.split('.')
 
         try:
-            for key in self.topic_subscriber_map.keys():
-                if topic in key or key in topic:
-                    return self.topic_subscriber_map[key]
+            for key, value in self.topic_subscriber_map.items():
+                key_parts = key.split('.')
+                if len(key_parts) <= len(topic_parts) and key_parts == topic_parts[:len(key_parts)]:
+                    subscribers.update(value)
         except Exception as e:
             logger.error(f"Error getting subscribers for topic: {topic}")
             logger.error(e)
-            return []
+
+        return list(subscribers)
 
 
 class MessageRouterActor:
---
+++ examples/tests/test_dramatiq_publisher.py
@@ -0,0 +1,35 @@
+import dramatiq
+from .common import worker
+import pytest
+
+
+@pytest.fixture
+def router_data():
+    from examples.dramatiq_pub_sub_ui.dramatiq_pub_sub_helpers import MessageRouterData
+    return MessageRouterData()
+
+
+def test_publish_message_can_send_messages_to_actors():
+    # this is adapted from the dramatiq testing suite
+
+    from examples.dramatiq_pub_sub_ui.dramatiq_pub_sub_helpers import publish_message
+
+    # Given that I have a database
+    database = {}
+
+    # And an actor that can write data to that database
+    @dramatiq.actor
+    def put(key, value):
+        database[key] = value
+
+    # If I publish a message to the actor
+    publish_message("test", "test", "put")
+
+    # And I give the workers time to process the messages
+    with worker(dramatiq.get_broker(), worker_timeout=100):
+        dramatiq.get_broker().join("default")
+
+    # I expect the database to be populated
+    assert database == {"test": "test"}
+
+
