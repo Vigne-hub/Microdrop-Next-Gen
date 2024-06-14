ee42aac960f8cdefe0f934bc10f65698c5405e70
MJAquarion2413 <90398733+MJAquarion2413@users.noreply.github.com>
2024-06-14 04:07:40 -0400

    Staging for dramatiq based pub sub. (#75)
    * Current relevant files for Dropbot actor and Event Hub actor restructure and Pub sub management.
    
    Note: Bundled since the overall structure will be overwritten in some respects with vigne-hub PR soon.
    
    * Current relevant files for Dropbot actor and Event Hub actor restructure and Pub sub management.
    
    Note: Bundled since the overall structure will be overwritten in some respects with vigne-hub PR soon.

--- microdrop/pydantic_models/base_signal_model.py
+++ microdrop/pydantic_models/base_signal_model.py
@@ -4,6 +4,6 @@ from pydantic import BaseModel
 class SignalNameModel(BaseModel):
     """ Pydantic model for Dropbot signal names"""
     Signal: str
-    Arg1: type
-    Arg2: type
-    Arg3: type  # etc....
+    Arg1: str
+    Arg2: str
+    Arg3: str  # etc....
