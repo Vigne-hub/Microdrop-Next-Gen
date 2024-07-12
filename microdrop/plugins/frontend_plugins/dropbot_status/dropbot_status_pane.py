35f3c701e3b5cbd3dc56bd426188065125fccb0e
Miguel <Miguel.Weerasinghe@mail.utoronto.ca>
2024-07-12 13:04:54 -0400

    DropBot Status Plugin View Pane

---
+++ microdrop/plugins/frontend_plugins/dropbot_status/dropbot_status_pane.py
@@ -0,0 +1,23 @@
+# enthought imports
+from pyface.tasks.dock_pane import DockPane
+
+# local imports
+from microdrop.plugins.frontend_plugins.dropbot_status.dropbot_status_widget import DropBotControlWidget
+
+# This module's package.
+PKG = '.'.join(__name__.split('.')[:-1])
+
+
+class DropbotStatusDockPane(DockPane):
+    """
+    A dock pane to set the voltage and frequency of the dropbot device.
+    """
+    #### 'ITaskPane' interface ################################################
+
+    id = PKG + ".dropbot_status"
+    name = "Dropbot Status"
+
+    #### 'ManualControlsPane' interface ##########################################
+
+    def create_contents(self, parent):
+        return DropBotControlWidget()
