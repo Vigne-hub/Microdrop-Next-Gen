a3ac3afb124c67492164bc5b5afb6958b6505fbc
Vigneshwar Rajesh <vignesh.rajesh@outlook.com>
2024-11-05 18:51:28 -0500

    Fixing constants values for the various plugins

--- dropbot_controller/consts.py
+++ dropbot_controller/consts.py
@@ -10,22 +10,24 @@ DROPBOT_DB3_120_HWID = 'VID:PID=16C0:0483'
 # `OUTPUT_ENABLE_PIN`.
 OUTPUT_ENABLE_PIN = 22
 
-# publishers
+# Topics actor declared by plugin subscribes to
 ACTOR_TOPIC_DICT = {
-    "dropbot_backend_listener": [
+    "dropbot_controller_listener": [
                                  "dropbot/requests/#",
                                  "dropbot/signals/disconnected",
                                  "dropbot/signals/halted"
     ]}
 
-# subscribers
+# Topics published by this plugin
 NO_DROPBOT_AVAILABLE = 'dropbot/signals/connection/warnings/no_dropbot_available'
 NO_POWER = 'dropbot/signals/connection/warnings/no_power'
+HALTED = 'dropbot/signals/halted'
+
 CHIP_NOT_INSERTED = 'dropbot/signals/chip_not_inserted'
 CHIP_INSERTED = 'dropbot/signals/chip_inserted'
-HALTED = 'dropbot/signals/halted'
-CAPACITANCE_UPDATED = 'dropbot/signals/capacitance_updated'
 SHORTS_DETECTED = 'dropbot/signals/shorts_detected'
+
+CAPACITANCE_UPDATED = 'dropbot/signals/capacitance_updated'
 
 # Dropbot Services Topics -- Offered by default from the dropbot monitor mixin in this package
 START_DEVICE_MONITORING = "dropbot/requests/start_device_monitoring"
