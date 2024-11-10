0b52601ea1c61653800830ebabe3e7c675b7fdf6
Vigneshwar Rajesh <vignesh.rajesh@outlook.com>
2024-11-09 22:20:37 -0500

    fix topic for actuation request.

--- electrode_controller/consts.py
+++ electrode_controller/consts.py
@@ -2,4 +2,4 @@
 PKG = '.'.join(__name__.split('.')[:-1])
 
 # Plugin offers services that are executed on publishing to this topic
-ELECTRODE_STATE_CHANGE = "dropbot/requests/start_device_monitoring"
+ELECTRODES_STATE_CHANGE = 'dropbot/requests/electrodes_state_change'
