509f4fa9a6f556638f245ca01fcc3cb2200ecf97
Vigneshwar Rajesh <vignesh.rajesh@outlook.com>
2024-11-09 22:56:58 -0500

    Create new method test case to try to implement. We need the boolean mask based on actuated channels and total available dropbot channels.

--- electrode_controller/tests/test_models.py
+++ electrode_controller/tests/test_models.py
@@ -1,5 +1,6 @@
 import re
 
+import numpy as np
 import pytest
 from traits.api import TraitError
 from ..models import ElectrodeStateChangeRequestMessageModel
@@ -19,7 +20,37 @@ def test_message_model_failure_non_boolean_value():
     """Test that MessageModel raises TraitError for JSON with non-boolean values."""
     json_data = '{"1": true, "2": "false"}'  # Value "false" is a string, not a boolean
 
-    with pytest.raises(TraitError, match=re.escape("JSON Message input should be a dictionary with string representation of "
-                                         "integer (numeric string) keys and Boolean values.")):
-
+    with pytest.raises(TraitError,
+                       match=re.escape("JSON Message input should be a dictionary with string representation of "
+                                       "integer (numeric string) keys and Boolean values.")):
         ElectrodeStateChangeRequestMessageModel(json_message=json_data)
+
+
+def test_get_boolean_channels_states_mask():
+    """Test that MessageModel returns the correct mask for the channels and states based on the input json string and
+    max available channels specified
+    """
+    json_data = '{"0": true, "1": false, "9": true}'
+    max_channels = 10
+
+    # Instantiate the model and validate
+    model = ElectrodeStateChangeRequestMessageModel(json_message=json_data, num_available_channels=max_channels)
+    assert np.all(
+        model.channels_states_boolean_mask ==
+        np.array([True, False, False, False, False, False, False, False, False, True])
+    )
+
+
+def test_get_boolean_channels_states_mask_zero_on():
+    """Test that MessageModel returns the correct mask for the channels and states based on the input json string and
+    max available channels specified
+    """
+    json_data = '{"0": false, "1": false, "9": false}'
+    max_channels = 10
+
+    # Instantiate the model and validate
+    model = ElectrodeStateChangeRequestMessageModel(json_message=json_data, num_available_channels=max_channels)
+    assert np.all(
+        model.channels_states_boolean_mask ==
+        np.array([False, False, False, False, False, False, False, False, False, False])
+    )
