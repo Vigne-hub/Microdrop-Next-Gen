8534dc8a42207a1273198dbbfbcdb8ca3e6d6071
Vigneshwar Rajesh <vignesh.rajesh@outlook.com>
2024-11-13 15:20:22 -0500

    Consolidate example running script. Just one is really needed. We can make additional ones just by exluding some plugins.

--- examples/run_device_viewer_pluggable.py
+++ examples/run_device_viewer_pluggable.py
@@ -1,25 +1,34 @@
 # Plugin imports.
-import sys
 
 from envisage.api import CorePlugin
 from envisage.ui.tasks.api import TasksPlugin
-
-from device_viewer.application import DeviceViewerApplication
-from device_viewer.plugin import DeviceViewerPlugin
+from microdrop_utils.broker_server_helpers import dramatiq_broker_context
 
 
 def main(args):
     """Run the application."""
 
-    plugins = [CorePlugin(), TasksPlugin(), DeviceViewerPlugin()]
+    from device_viewer.application import DeviceViewerApplication
+    from device_viewer.plugin import DeviceViewerPlugin
+    from dropbot_status.plugin import DropbotStatusPlugin
+    from message_router.plugin import MessageRouterPlugin
+    from dropbot_controller.plugin import DropbotControllerPlugin
+    from manual_controls.plugin import ManualControlsPlugin
+    from electrode_controller.plugin import ElectrodeControllerPlugin
+
+    plugins = [CorePlugin(), TasksPlugin(), DeviceViewerPlugin(), DropbotStatusPlugin(),
+               MessageRouterPlugin(), DropbotControllerPlugin(), ElectrodeControllerPlugin(), ManualControlsPlugin()]
+
     app = DeviceViewerApplication(plugins=plugins)
-    app.run()
+
+    with dramatiq_broker_context():
+        app.run()
 
 
 if __name__ == "__main__":
     import sys
-
     import os
+
     sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
     main(sys.argv)
