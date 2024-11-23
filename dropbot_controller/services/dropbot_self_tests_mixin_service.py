cfb09d9dd11a8b859fc5ac0bd91f98ae157ea88e
Vigneshwar Rajesh <vignesh.rajesh@outlook.com>
2024-11-23 18:47:36 -0500

    - Create a decorator to execute self tests based on function name.

--- dropbot_controller/services/dropbot_self_tests_mixin_service.py
+++ dropbot_controller/services/dropbot_self_tests_mixin_service.py
@@ -1,17 +1,18 @@
 from pathlib import Path
+from functools import wraps
+import datetime as dt
 
 from traits.api import provides, HasTraits
 from dropbot import self_test
-import datetime as dt
 
 from microdrop_utils._logger import get_logger
 
-from ..interfaces.i_dropbot_control_mixin_service import IDropbotControlMixinService
-
 logger = get_logger(__name__)
 
+from ..interfaces.i_dropbot_control_mixin_service import IDropbotControlMixinService
 
-def get_timestamped_path(path: [str, Path]):
+
+def get_timestamped_results_path(test_name: str, path: [str, Path]):
     """
     Simple function to add datestamp to a given path
     """
@@ -22,7 +23,7 @@ def get_timestamped_path(path: [str, Path]):
     # Generate unique filename
     timestamp = dt.datetime.utcnow().strftime('%Y-%m-%dT%H_%M_%S')
 
-    return path.joinpath(f'results-{timestamp}')
+    return path.joinpath(f'{test_name}_results-{timestamp}')
 
 
 @provides(IDropbotControlMixinService)
@@ -34,14 +35,72 @@ class DropbotSelfTestsMixinService(HasTraits):
     id = "dropbot_self_tests_mixin_service"
     name = 'Dropbot Self Tests Mixin'
 
+    ######################################## private methods ##############################################
+
+    @staticmethod
+    def _execute_test_based_on_name(func):
+        @wraps(func)
+        def _execute_test(self, report_generation_directory):
+            """
+            Method to execute a dropbot test based on the name
+            """
+
+            # find the required test name based on the function name "on_testname_request"
+            test_name = "_".join(func.__name__.split('_')[1:-1])
+
+            # set the report file name in the needed dir based on tests run
+            report_path = f"{get_timestamped_results_path(test_name, report_generation_directory)}.html"
+
+            # the tests arg should be None for self test if all tests need to be run
+            if test_name == "run_all_tests":
+                tests = None
+            else:
+                tests = [test_name]
+
+            logger.info(f"Running test {test_name}")
+            result = self_test.self_test(self.proxy, tests=tests)
+
+            logger.info(f"Report generating in the file {report_path}")
+            self_test.generate_report(result, report_path, force=True)
+
+            # do whatever else is defined in func
+            func(self, report_generation_directory)
+
+        return _execute_test
+
     ######################################## Methods to Expose #############################################
+
+    @_execute_test_based_on_name
     def on_run_all_tests_request(self, report_generation_directory: str):
         """
-        Method to start looking for dropbots connected using their hwids.
+        Method to run all dropbot hardware tests
         """
+        pass
 
-        report_path = f"{get_timestamped_path(report_generation_directory)}.html"
+    @_execute_test_based_on_name
+    def on_test_voltage_request(self, report_generation_directory: str):
+        """
+        Method to run the high voltage dropbot test
+        """
+        pass
 
-        result = self_test.self_test(self.proxy)
+    @_execute_test_based_on_name
+    def on_test_on_board_feedback_calibration_request(self, report_generation_directory: str):
+        """
+        Method to run the On-Board feedback calibration test.
+        """
+        pass
 
-        self_test.generate_report(result, report_path, force=True)
+    @_execute_test_based_on_name
+    def on_test_shorts_request(self, report_generation_directory: str):
+        """
+        Method to run the shorted channels test.
+        """
+        pass
+
+    @_execute_test_based_on_name
+    def on_test_channels_request(self, report_generation_directory: str):
+        """
+        Method to run the test board scan.
+        """
+        pass
