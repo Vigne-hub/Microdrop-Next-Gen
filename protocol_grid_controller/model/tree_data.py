10b1a0e6158eb343bbb6edbd061f486759bbb509
Vigneshwar Rajesh <vignesh.rajesh@outlook.com>
2025-04-25 17:35:50 -0400

    Change string indexing to more systematic array based indexing for tree elements.

--- protocol_grid_controller/model/tree_data.py
+++ protocol_grid_controller/model/tree_data.py
@@ -1,87 +1,52 @@
 from pydantic import BaseModel, Field, validator
-from typing import List, Optional, Dict, Any
+from typing import List, Optional, Dict, Any, Tuple, Union, TYPE_CHECKING
 import json
 
 
 # Define the Step model
 class ProtocolStep(BaseModel):
-    id: str = ""
+    idx: List[int] = [0]
     name: str
     parameters: Dict[str, Any]
 
 
 # Define the Group model
 class ProtocolGroup(BaseModel):
-    group_id: str = ""
-    group_name: str
+    group_idx: List[int] = [0]
+    name: str
 
-    steps: Optional[List[ProtocolStep]] = Field(default_factory=list)
-    sub_groups: Optional[List['ProtocolGroup']] = Field(default_factory=list)
+    elements: Optional[List[Union[ProtocolStep, 'ProtocolGroup']]] = Field(default_factory=list)
 
     def __init__(self, **data):
         super().__init__(**data)
 
     @property
-    def id(self):
-        return self.group_id
+    def idx(self):
+        return self.group_idx
 
-    @id.setter
-    def id(self, value):
-        self.group_id = value
+    @idx.setter
+    def idx(self, value):
+        self.group_idx = value
 
         # this group has been added to a super group. update its subgroups, and steps to reflect the new structure
-        for sub_group in self.sub_groups:
-            sub_group.id = f"{value}.{sub_group.id}"
+        for element in self.elements:
+            element.idx = self.idx + element.idx[:-1]
 
-        for steps in self.steps:
-            steps.id = f"{value}.{steps.id}"
+    def add_element(self, new_element: [ProtocolStep, 'ProtocolGroup']):
+        new_element.idx = self.idx + [len(self.elements)]
+        self.elements.append(new_element)
 
-    def add_step(self, new_step: ProtocolStep):
-        new_step_id = f"step{len(self.steps) + 1}"
+    def get_element(self, element_idx: str):
+        for element in self.elements:
+            # Direct match
+            if element.idx == element_idx:
+                return element
 
-        # add the self id to the subgroup id if it exists.
-        if self.id:
-            new_step.id = f"{self.id}.{new_step_id}"
-        else:
-            new_step.id = new_step_id
+            # Recurse into subgroups
+            elif isinstance(element, ProtocolGroup):
+                sub_element = element.get_element(element_idx)
+                if sub_element is not None:
+                    return sub_element
 
-        self.steps.append(new_step)
-
-    def add_sub_group(self, new_subgroup: 'ProtocolGroup'):
-        sub_group_id = f"group{len(self.sub_groups) + 1}"
-
-        # add the self id to the subgroup id if it exists.
-        if self.id:
-            new_subgroup.id = f"{self.id}.{sub_group_id}"
-        else:
-            new_subgroup.id = sub_group_id
-
-        self.sub_groups.append(new_subgroup)
-
-    def update_step_parameters(self, step_id: str, new_parameters: Dict[str, Any]):
-        for step in self.steps:
-            if step.id == step_id:
-                step.parameters.update(new_parameters)
-                return
-
-        for sub_group in self.sub_groups:
-            sub_group.update_step_parameters(step_id, new_parameters)
-
-    def get_step(self, step_id: str) -> Optional[ProtocolStep]:
-        for step in self.steps:
-            if step.id == step_id:
-                return step
-        for sub_group in self.sub_groups:
-            found_step = sub_group.get_step(step_id)
-            if found_step:
-                return found_step
-        return None
-
-    def get_group(self, group_id: str) -> Optional['ProtocolGroup']:
-        if self.id == group_id:
-            return self
-        for sub_group in self.sub_groups:
-            found_group = sub_group.get_group(group_id)
-            if found_group:
-                return found_group
+        # If we never returned, the element was not found
         return None
--- protocol_grid_controller/tests/test_tree_data_model.py
+++ protocol_grid_controller/tests/test_tree_data_model.py
@@ -7,37 +7,37 @@ from protocol_grid_controller.model.tree_data import ProtocolGroup, ProtocolStep
 
 @pytest.fixture
 def protocol_tree():
-    root = ProtocolGroup(group_id="root", group_name="Root Group")
-    step1 = ProtocolStep(name="Step1", parameters={"param1": "value1"})
-    root.add_step(step1)
+    root = ProtocolGroup(name="Root Group")  # [0]
+    step1 = ProtocolStep(name="Step1", parameters={"param1": "value1"})  # [0, 0]
+    root.add_element(step1)
 
     # Level 1
-    sub_group = ProtocolGroup(group_name="Level 1")
-    step2 = ProtocolStep(name="Step2", parameters={"param2": "value2"})
-    sub_group.add_step(step2)
-    root.add_sub_group(sub_group)
+    sub_group = ProtocolGroup(name="Level 1")  # [0, 1]
+    step2 = ProtocolStep(name="Step2", parameters={"param2": "value2"})  # [0, 1, 0]
+    sub_group.add_element(step2)
+    root.add_element(sub_group)
 
     # Level 2
-    sub_sub_group = ProtocolGroup(group_name="Level 2")
-    step3 = ProtocolStep(name="Step3", parameters={"param3": "value3"})
-    sub_group.add_sub_group(sub_sub_group)
-    sub_sub_group.add_step(step3)
+    sub_sub_group = ProtocolGroup(name="Level 2")  # [0, 1, 1]
+    step3 = ProtocolStep(name="Step3", parameters={"param3": "value3"})  # [0, 1, 1, 0]
+    sub_group.add_element(sub_sub_group)
+    sub_sub_group.add_element(step3)
 
-    # Additional steps
+    # Add additional steps
     new_step1 = ProtocolStep(name="newStep1", parameters={"newparam2": "value2", "newparam3": "value3"})
-    root.add_step(new_step1)
+    root.add_element(new_step1)  # [0, 2]
 
     new_step2 = ProtocolStep(name="NewStep2", parameters={"Newparam2": "Newvalue2"})
-    sub_group.add_step(new_step2)
+    sub_group.add_element(new_step2)  # [0, 1, 2]
 
-    new_step3 = ProtocolStep(name="NewStep3", parameters={"Newparam3": "Newvalue3"})
-    root.get_group(group_id="root.group1.group1").add_step(new_step3)
+    new_step3 = ProtocolStep(name="NewStep3", parameters={"Newparam3": "Newvalue3"})  # [0,1,1,1]
+    root.get_element(element_idx=[0, 1, 1]).add_element(new_step3)
 
-    # Parallel subgroup
-    level2_group2 = ProtocolGroup(group_name="Level 2, group 2")
-    level2_group2.add_step(step1.model_copy(update={"name": step1.name + "_copy"}))
-    level2_group2.add_step(new_step1.model_copy(update={"name": new_step1.name + "_copy"}))
-    root.get_group(group_id="root.group1").add_sub_group(level2_group2)
+    # Add multiple groups at same level
+    level2_group2 = ProtocolGroup(name="Level 2, group 2")
+    level2_group2.add_element(step1.model_copy(update={"name": step1.name + "_copy"}))
+    level2_group2.add_element(new_step1.model_copy(update={"name": new_step1.name + "_copy"}))
+    root.get_element([0, 1]).add_element(level2_group2)
 
     return root
 
@@ -48,6 +48,11 @@ def test_json_serialization_matches_snapshot(protocol_tree):
         expected = json.load(f)
     actual = json.loads(json_model)
     assert actual == expected
+
+def test_tree_traversal(protocol_tree):
+    check_group = protocol_tree.get_element([0, 1, 3])
+
+    assert check_group.name == "Level 2, group 2"
 
 
 if __name__ == "__main__":
