789f4ffdd87385d6d9d311e51c8dfd37655fa466
Alexandros Sklavounos <alex_sklavounos@yahoo.gr>
2025-05-20 18:51:49 -0400

    Fix issue when the window is has not yet been created

--- dropbot_tools_menu/plugin.py
+++ dropbot_tools_menu/plugin.py
@@ -53,7 +53,23 @@ class DropbotToolsMenuPlugin(Plugin):
 
     @observe("application:application_initialized")
     def on_application_initialized(self, event):
-        # add some listener methods to the application task
+        # Wait for the window to be created
+        if self.application.active_window is None:
+            # If window is not created yet, observe the window creation
+            self.application.on_trait_change(self._on_window_created, 'active_window')
+            return
+            
+        self._setup_task_listeners()
+
+    def _on_window_created(self, window):
+        """Called when the application window is created."""
+        if window is not None:
+            self._setup_task_listeners()
+            # Remove the observer since we don't need it anymore
+            self.application.on_trait_change(self._on_window_created, 'active_window', remove=True)
+
+    def _setup_task_listeners(self):
+        """Set up the task listeners once the window is available."""
         for task in self.application.active_window.tasks:
             if task.id == f"{device_viewer_PKG}.task":
                 setattr(task, _on_self_tests_progress_triggered.__name__,
