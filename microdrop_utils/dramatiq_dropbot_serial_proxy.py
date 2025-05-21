be4ff0acbc8255ea210b39783c0563b0e9da94b8
numberisnan <github@farazkaleemmalik.cyou>
2025-05-21 10:28:22 -0400

    Undo call to super().connect()

--- microdrop_utils/dramatiq_dropbot_serial_proxy.py
+++ microdrop_utils/dramatiq_dropbot_serial_proxy.py
@@ -12,7 +12,7 @@ connection_flags = {"connected": CONNECTED, "disconnected": DISCONNECTED}
 class DramatiqDropbotSerialProxy(SerialProxy):
 
     def connect(self):
-        monitor = super().connect()
+        self.terminate()
 
         # We need to signal to the dramatiq pub sub system that the dropbot has been disconnected/connected we are
         # writing a wrapper will still do everything the original monitor function does except for the dramatiq pub
@@ -23,7 +23,13 @@ class DramatiqDropbotSerialProxy(SerialProxy):
             f()
             publish_message(f'dropbot_{signal_name}', connection_flags[signal_name])
 
+        monitor = bnr.ser_async.BaseNodeSerialMonitor(port=self.port)
+
         monitor.connected_event.set = ft.partial(publish_wrapper, monitor.connected_event.set, 'connected')
         monitor.disconnected_event.set = ft.partial(publish_wrapper, monitor.disconnected_event.set, 'disconnected')
 
-        return monitor
+        monitor.start()
+
+        monitor.connected_event.wait()
+        self.monitor = monitor
+        return self.monitor
