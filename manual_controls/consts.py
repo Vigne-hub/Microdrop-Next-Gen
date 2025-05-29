4c768328140fc87a44fe4931c221275bae2e0b15
Vigneshwar Rajesh <vignesh.rajesh@outlook.com>
2025-05-29 16:25:24 -0400

    dropbot setup success not hardcoded

--- manual_controls/consts.py
+++ manual_controls/consts.py
@@ -1,11 +1,12 @@
 # This module's package.
 import os
+from dropbot_controller.consts import DROPBOT_SETUP_SUCCESS
 
 PKG = '.'.join(__name__.split('.')[:-1])
 PKG_name = PKG.title().replace("_", " ")
 
 # Topics actor declared by plugin subscribes to
 ACTOR_TOPIC_DICT = {
-    f"{PKG}_listener": ["dropbot/signals/setup_success"]}
+    f"{PKG}_listener": [DROPBOT_SETUP_SUCCESS]}
 
 HELP_PATH = os.path.join(os.path.dirname(__file__), "help")
